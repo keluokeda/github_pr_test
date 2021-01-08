7ec07bb6cf85771de5edc77dbae1781d5aa78f91
余艳辉 <913918146@qq.com>
2021-01-08 14:02:45 +0800

    Initial commit

---
+++ app/src/main/java/com/ke/github_pr_test/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ke.github_pr_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun showAlertDialog() {
+        //等待别人来贡献完成
+    }
+
+   
+}
